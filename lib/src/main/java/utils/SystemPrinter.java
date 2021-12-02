ee222a3a34084fa2004b7ea9d9f204257d4366e5
byc <byckg3@gmail.com>
2021-12-03 04:39:09 +0800

    add SystemPrinter

---
+++ lib/src/main/java/utils/SystemPrinter.java
@@ -0,0 +1,32 @@
+package utils;
+
+import java.util.List;
+import java.util.Map;
+
+public class SystemPrinter
+{
+    public static < E > void println( List< E > list )
+    {
+        StringBuilder elements = new StringBuilder( "[" );
+        for ( E eachElement: list )
+        {
+            elements.append( " " + eachElement.toString() );
+        }
+        elements.append( " ]" );
+
+        System.out.println( elements.toString() );
+    }
+
+    public static < K, V > void println( Map< K, V > map )
+    {
+        StringBuilder entries = new StringBuilder();
+        for ( var eachEntry: map.entrySet() )
+        {
+            entries.append( eachEntry.getKey() );
+            entries.append( ":\t" );
+            entries.append( eachEntry.getValue() );
+            entries.append( "\n" );
+        }
+        System.out.println( entries.toString() );
+    }
+}
